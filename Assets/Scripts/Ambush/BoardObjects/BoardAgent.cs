8b9689d062c2347b0681d2c1ac09fd560545fc5d
notbloom <orlando.bascunan@gmail.com>
2021-11-08 15:39:05 -0300

    fix

---
+++ Assets/Scripts/Ambush/BoardObjects/BoardAgent.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using notbloom.HexagonalMap;
+namespace Ambush
+{
+    
+    public enum StatModifierType {
+        maxHealthModifier, movementModifier, physicalDamageModifier, magicalDamageModifier
+    }
+    public class StatModifier {
+        public StatModifierType type;
+        public ushort value;
+    }
+    public enum StatType
+    {
+        movement, step, physicalDamage, magicalDamage, pierce, armor, maxHp, currentHp
+    }
+
+    public class Stat
+    {
+        public StatType type;
+        public int value;
+    }
+
+    public enum SpecialAbilityTrigger { OnTurnStart, OnTurnEnd, OnDamagePlayer, OnDamageEnemy, OnKillEnemy, }
+    public class SpecialAbility
+    {
+
+    }
+    public class Equipment
+    {
+        public string name;
+        public List<Stat> stats;
+        //public void Equip(BoardAgent boardAgent)
+        //{
+        //    boardAgent.maxHealth += 10;
+        //}
+        //public void Unequip(BoardAgent boardAgent)
+        //{
+        //    boardAgent.maxHealth -= 10;
+        //}
+
+    }   
+    public class BoardAgent : BoardObject
+    {
+     //   public Node position;
+       // public BoardAgentStats baseStats;
+       // public Sprite boardSprite;
+        public string unique_id;
+        public string id; //name        
+        public List<Equipment> equipment;
+
+        // Stats
+        //initial_fixed_stats        
+        public ushort initiative;
+        public string readableName;
+
+        // Base Stats
+        public int baseMovement; // amount of steps per turn         
+        public int baseMaxHealth;
+        public int basePhysicalDamage; // adds physical damage
+        public int baseMagicalDamage; // adds magical damage        
+
+        //Usable Stats
+        public int movement; // amount of steps per turn         
+        public int maxHealth;
+        public int physicalDamage; // adds physical damage
+        public int magicalDamage; // adds magical damage
+        public int currentHealth;
+
+        public string Description() { return $"HP:{baseMaxHealth}"; }
+
+        public int GetMaxHealth() => maxHealth + GetStatModifier(StatType.maxHp);
+        public int GetMovement() => movement + GetStatModifier(StatType.movement);
+        public int GetMagicalDamage() => magicalDamage + GetStatModifier(StatType.magicalDamage);
+        public int GetPhysicalDamage() => physicalDamage + GetStatModifier(StatType.physicalDamage);
+
+        public int GetPhysicalDamage2()
+        {
+            int r = physicalDamage;
+
+            var o = from e in equipment
+                    from s in e.stats
+                    where s.type == StatType.physicalDamage
+                    select s.value;
+
+
+            foreach (Equipment e in equipment)
+            {
+                // e.stats.Where(s => s.type == StatType.physicalDamage).
+
+                foreach (Stat a in e.stats)
+                {
+                    if (a.type == StatType.physicalDamage)
+                    {
+                        r += a.value;
+                    }
+                }
+            }
+
+            return r;
+        }
+
+        public int GetStatModifier(StatType statType)
+        {
+            int r = 0;
+
+            var o = from e in equipment
+                    from s in e.stats
+                    where s.type == statType
+                    select s.value;
+
+            foreach (int i in o)
+            {
+                r += i;
+            }
+            return r;
+
+
+        }
+
+    }
+    public class BoardEnemy : BoardAgent
+    {
+
+    }
+}
---
+++ Assets/Scripts/Ambush/BoardObjects/BoardObjectRepository.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ambush{  
+    public class BoardObjectRepository : MonoBehaviour
+    {
+        // Use this for initialization
+        public ScriptableEnemy[] scriptableEnemies;
+        public Dictionary<string, ScriptableEnemy> GetScriptableEnemy;
+
+        private void Awake()
+        {
+            GetScriptableEnemy = new Dictionary<string, ScriptableEnemy>();
+            for (int i = 0; i < scriptableEnemies.Length; i++)
+            {
+                GetScriptableEnemy.Add(scriptableEnemies[i].name, scriptableEnemies[i]);
+            }
+        }
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}

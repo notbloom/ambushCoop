7709dc6a8d58404dd87338aaa60c34c4a70bec5a
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2021-11-24 01:59:41 +0100

    Comment;

--- Assets/Scripts/Ambush/Skills/Actions/IActionController.cs
+++ Assets/Scripts/Ambush/Skills/Actions/IActionController.cs
@@ -4,13 +4,22 @@ namespace Ambush
 {	
     public interface IActionController{
 		int Cost();
+
+		//En la barra
 		void OnSkillHover(PlayerBehaviour playerBehaviour);
 		void OnSkillExitHover(PlayerBehaviour playerBehaviour);
+
+		//apretarla de la barra
 		void OnSkillActivate(PlayerBehaviour playerBehaviour);
+
+		//arrepentio
 		void OnSkillCancel(PlayerBehaviour playerBehaviour);
+        
+        //que es lo que va a pasar en el nodo en el tablero
 		void OnNodeEnter(PlayerBehaviour playerBehaviour, Node node);
 		void OnNodeExit(PlayerBehaviour playerBehaviour, Node node);
-		void OnNodePress(PlayerBehaviour playerBehaviour, Node node);
 
+		//Queremos que lo ejecute y su animacion;
+		void OnNodePress(PlayerBehaviour playerBehaviour, Node node);
     }
 }

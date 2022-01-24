aeb257f80698a8c3677e75150b4da0e0c9d94153
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2022-01-24 23:22:05 +0100

    comment out the rest of the shit

--- Assets/Scripts/Characters/ScriptableObjects/Actions/BaseAttackAction.cs
+++ Assets/Scripts/Characters/ScriptableObjects/Actions/BaseAttackAction.cs
@@ -1,38 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using notbloom.HexagonalMap;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using notbloom.HexagonalMap;
 
-[CreateAssetMenu(fileName = "new BaseAttackAction", menuName = "TurnActions/BaseAttackAction", order = 0)]
-public class BaseAttackAction : ScriptableAction
-{
-    // public int targets;
-    // public float damage;
-    // public int range;
-    // public List<Status> statuses;
+//[CreateAssetMenu(fileName = "new BaseAttackAction", menuName = "TurnActions/BaseAttackAction", order = 0)]
+//public class BaseAttackAction : ScriptableAction
+//{
+//    // public int targets;
+//    // public float damage;
+//    // public int range;
+//    // public List<Status> statuses;
 
-    public NodeAnimationFactory animationFactory;
-    public override void PerformAction(HNode from, List<HNode> targets, AgentBase agent)
-    {
-        //   Debug.Log("attacking cleave");
-        foreach (HNode target in targets)
-        {
-            //     Debug.Log(target.ToString());
-            if (target.occupant != null)
-            {
-                //        Debug.Log("occupant found");
-                if (target.occupant.agent != null)
-                {
-                    //          Debug.Log("agent not null");
+//    public NodeAnimationFactory animationFactory;
+//    public override void PerformAction(HNode from, List<HNode> targets, AgentBase agent)
+//    {
+//        //   Debug.Log("attacking cleave");
+//        foreach (HNode target in targets)
+//        {
+//            //     Debug.Log(target.ToString());
+//            if (target.occupant != null)
+//            {
+//                //        Debug.Log("occupant found");
+//                if (target.occupant.agent != null)
+//                {
+//                    //          Debug.Log("agent not null");
 
-                    HDamageInstance damageInstance = new HDamageInstance(agent.agent.attack);
-                    DamageAnimationFactory.damageInstance = damageInstance;
-                    //TODO create a DamageAnimationFactory
-//                    AnimationInvoker.Enqueue(animationFactory.Generate(agent.gameObject, new List<HNode>() { from }, new List<HNode>() { target }, 0.2f));
-                    target.occupant.agent.ReceiveDamage(damageInstance);
-                }
-            }
-        }
-        //   Debug.Log("attacked cleave");
-    }
-}
+//                    HDamageInstance damageInstance = new HDamageInstance(agent.agent.attack);
+//                    DamageAnimationFactory.damageInstance = damageInstance;
+//                    //TODO create a DamageAnimationFactory
+////                    AnimationInvoker.Enqueue(animationFactory.Generate(agent.gameObject, new List<HNode>() { from }, new List<HNode>() { target }, 0.2f));
+//                    target.occupant.agent.ReceiveDamage(damageInstance);
+//                }
+//            }
+//        }
+//        //   Debug.Log("attacked cleave");
+//    }
+//}
--- Assets/Scripts/Characters/ScriptableObjects/TargetRequest/RequestTargetInRange.cs
+++ Assets/Scripts/Characters/ScriptableObjects/TargetRequest/RequestTargetInRange.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using notbloom.HexagonalMap;
-using System.Linq;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using notbloom.HexagonalMap;
+//using System.Linq;
 
-[CreateAssetMenu(fileName = "RequestTargetInRange", menuName = "TurnActions/RequestTarget/RequestTargetInRange", order = 0)]
+//[CreateAssetMenu(fileName = "RequestTargetInRange", menuName = "TurnActions/RequestTarget/RequestTargetInRange", order = 0)]
 
-//TODO IMPLEMENT THIS, IT'S ONLY GRABBING ANY TARGET
-public class RequestTargetInRange : ScriptableTargetRequest
-{
-    public override List<HNode> Request(List<HNode> range, ScriptableArea area, HObjectFactions targetFaction)
-    {
-        List<HNode> targets = range
-        .Where(o => o.occupant != null)
-        .Where(p => p.occupant.faction == targetFaction)
-        .ToList();
-        return targets;
-    }
+////TODO IMPLEMENT THIS, IT'S ONLY GRABBING ANY TARGET
+//public class RequestTargetInRange : ScriptableTargetRequest
+//{
+//    public override List<HNode> Request(List<HNode> range, ScriptableArea area, HObjectFactions targetFaction)
+//    {
+//        List<HNode> targets = range
+//        .Where(o => o.occupant != null)
+//        .Where(p => p.occupant.faction == targetFaction)
+//        .ToList();
+//        return targets;
+//    }
 
-}
+//}

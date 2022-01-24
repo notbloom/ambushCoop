47ef78a5e684bb9a4a246d81bbfd443f1aaf1c79
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2022-01-24 23:22:48 +0100

    recompila wom

--- Assets/Scripts/Characters/ScriptableObjects/TargetRequest/RequestAnyTargetOfTypeInArea.cs
+++ Assets/Scripts/Characters/ScriptableObjects/TargetRequest/RequestAnyTargetOfTypeInArea.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using notbloom.HexagonalMap;
-using System.Linq;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using notbloom.HexagonalMap;
+//using System.Linq;
 
-//TODO CHANGE SPELLING TO INRANGE
-public class RequestAnyTargetOfTypeInArea : ScriptableTargetRequest
-{
-    public override List<HNode> Request(List<HNode> range, ScriptableArea area, HObjectFactions targetFaction)
-    {
-        return range.Where(x => x.occupant.faction == targetFaction).ToList();
-    }
+////TODO CHANGE SPELLING TO INRANGE
+//public class RequestAnyTargetOfTypeInArea : ScriptableTargetRequest
+//{
+//    public override List<HNode> Request(List<HNode> range, ScriptableArea area, HObjectFactions targetFaction)
+//    {
+//        return range.Where(x => x.occupant.faction == targetFaction).ToList();
+//    }
 
-}
+//}
